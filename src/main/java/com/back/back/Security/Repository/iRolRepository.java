25871844e65a4e02a5a8e997b5f73c3dea7b2d47
EnzoGaticaOdato <enzogaticaodato@gmail.com>
2023-06-03 14:57:53 -0700

    Back end para login funcionando

---
+++ src/main/java/com/back/back/Security/Repository/iRolRepository.java
@@ -0,0 +1,12 @@
+package com.back.back.Security.Repository;
+
+import com.back.back.Security.Entity.Rol;
+import com.back.back.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer> {
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
