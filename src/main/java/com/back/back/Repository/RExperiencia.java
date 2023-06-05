44639165b737c13ef36cb25c361f3096d5655b03
EnzoGaticaOdato <enzogaticaodato@gmail.com>
2023-06-05 01:39:02 -0700

    Experiencia funcionado (CRUD)

---
+++ src/main/java/com/back/back/Repository/RExperiencia.java
@@ -0,0 +1,12 @@
+package com.back.back.Repository;
+
+import com.back.back.Entity.Experiencia;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RExperiencia extends JpaRepository<Experiencia, Integer>{
+    public Optional<Experiencia> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
