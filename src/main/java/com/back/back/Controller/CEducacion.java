a138e748c6e569adc637385c0f36cd09a4a3536f
EnzoGaticaOdato <enzogaticaodato@gmail.com>
2023-06-08 17:03:07 -0700

    Cambio crossorigin por URL de firebase

--- src/main/java/com/back/back/Controller/CEducacion.java
+++ src/main/java/com/back/back/Controller/CEducacion.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://frontend-argentinaprograma2023.web.app","http://localhost:4200"})
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
--- src/main/java/com/back/back/Controller/CExperiencia.java
+++ src/main/java/com/back/back/Controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://frontend-argentinaprograma2023.web.app","http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
