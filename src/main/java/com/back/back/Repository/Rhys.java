3113a137f3f85e86d3c335030cae88010bdd49dd
EnzoGaticaOdato <enzogaticaodato@gmail.com>
2023-06-28 00:45:52 -0700

    Hand and soft para deploy

---
+++ src/main/java/com/back/back/Repository/Rhys.java
@@ -0,0 +1,11 @@
+package com.back.back.Repository;
+
+import com.back.back.Entity.hys;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Rhys extends JpaRepository<hys, Integer>{
+    Optional<hys> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+    
+}
